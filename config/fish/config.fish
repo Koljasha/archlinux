dcd0dd651c22be07a42644a07d033a37c5034cac
Koljasha <koljasha@mail.ru>
2021-01-14 17:20:30 +0300

    packages

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+#
+# Koljasha settings
+#
+
+set fish_greeting
+
+# aliases
+
+# linux ll command
+#
+function ll --description 'List contents of directory using long format'
+        ls -lah $argv
+end
+#
+###
+
