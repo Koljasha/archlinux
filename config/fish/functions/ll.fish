d29764a44a93ac9fc24ca3cd8d5355cfe464bb60
Koljasha <koljasha@mail.ru>
2021-01-14 19:21:02 +0300

    packages

--- config/fish/functions/ll.fish
+++ config/fish/functions/ll.fish
@@ -1,3 +1,4 @@
+# Define ls -lah function
 function ll --description 'List contents of directory using long format'
         ls -lah $argv
 end
---
+++ config/fish/functions/rcd.fish
@@ -0,0 +1,9 @@
+# Defined fish auto cd function
+function rcd
+	set dir (mktemp -t ranger_cd.XXX)
+    ranger --choosedir=$dir
+    cd (cat $dir) $argv
+    rm $dir
+    commandline -f repaint
+end
+
