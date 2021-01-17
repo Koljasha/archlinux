160131226e5e0ce7583d72de7f297d85c814f5f5
Koljasha <koljasha@mail.ru>
2021-01-17 12:37:56 +0300

    packages

--- config/polybar/i3.sh
+++ config/polybar/i3.sh
@@ -5,6 +5,10 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+# polybar - change display output
+disp=`xrandr | grep "connected primary" | cut -d" " -f1`
+sed -i "s/--output.*--brightness/--output $disp --brightness/" ~/.config/polybar/change_brightness.sh
+
 # Launch bars
 echo "---" | tee -a /tmp/polybar.log
 polybar -c ~/.config/polybar/i3.conf polybar >>/tmp/polybar.log 2>&1 &
