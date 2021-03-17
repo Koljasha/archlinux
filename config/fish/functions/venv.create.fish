9a82834b0283db04280417462da6209247807fd7
Koljasha <koljasha@mail.ru>
2021-03-17 10:15:57 +0300

    rm -> pip install wheel

--- config/fish/functions/venv.create.fish
+++ config/fish/functions/venv.create.fish
@@ -1,6 +1,6 @@
 # python virtualenv
 #
 function venv.create --description 'create and activate python virtual environment'
-     python -m venv venv; and venv.activate; and pip install wheel
+     python -m venv venv; and venv.activate
 end
 
