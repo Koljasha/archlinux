141b13d02d235e659b484ac53230bedc348b16a6
Koljasha <koljasha@mail.ru>
2021-09-30 15:45:46 +0300

    i3 add show_binding script

---
+++ config/i3/show_binding.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Show all active i3wm binding
+
+grep bindsym ~/.config/i3/config | egrep -v "^#" | egrep -v "^ "\
+    | sed 's/bindsym //' | sed 's/--no-startup-id //' | sed 's/exec //' | sed 's/ / -> /'\
+    | sed -r 's/^(.*) -> /\x1b[34m\1\x1b[0m -> /'\
+    | sed -r 's/ -> (.*)$/ -> \x1b[31m\1\x1b[0m/'
+
