d6e109bdb16d2a75b6710e9832672283a76d981e
Koljasha <koljasha@mail.ru>
2021-12-24 10:40:25 +0300

    fzf for fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,11 +3,12 @@
 #
 
 set fish_greeting
+# for fish fzf
+set -U FZF_COMPLETE 2
+# save current directory (for i3 terminal)
+prompt_command
 
 alias ll="ls -lah"
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
-
-# save current directory (need for i3 terminal)
-prompt_command
 
