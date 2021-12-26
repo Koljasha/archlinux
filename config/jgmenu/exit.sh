b4f036aa33baa4d1ea24e3d4d3da64f2f067edb4
Koljasha <koljasha@mail.ru>
2021-12-26 14:43:38 +0300

    jgmenu qtile exit

--- config/jgmenu/exit.sh
+++ config/jgmenu/exit.sh
@@ -6,5 +6,7 @@ if [[ -n $(pgrep -x openbox) ]]; then
     openbox --exit
 elif [[ -n $(pgrep -x i3) ]]; then
     i3-msg "exit"
+elif [[ -n $(pgrep -x qtile) ]]; then
+    qtile cmd-obj -o cmd -f shutdown
 fi
 
