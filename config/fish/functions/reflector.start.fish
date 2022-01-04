0d535dc822afeffd139b51abd5464bae8c843e28
Koljasha <koljasha@mail.ru>
2022-01-04 18:00:29 +0300

    fish reflector backup

--- config/fish/functions/reflector.start.fish
+++ config/fish/functions/reflector.start.fish
@@ -1,6 +1,7 @@
 # defined reflector.service start
 #
 function reflector.start --description 'reflector.service start'
+    sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
     sudo systemctl start reflector.service
 end
 
