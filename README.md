853e99b357b1a03456547bb8fedc1faaab3da18e
Koljasha <koljasha@mail.ru>
2022-08-10 15:42:50 +0300

    rm some function openvpn, charge

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 #### Ru localization is default; for change:
 * `./installer` lines : 11 - 12
 * `./chroot`    lines : 3 - 14
-* `./packages`  lines : 677 - 678
+* `./packages`  lines : 666 - 667
 
 #### Boot from [archlinux.iso](https://archlinux.org/download/), then:
 * `pacman -Sy git`
--- config/fish/functions/charge.fish
+++
@@ -1,7 +0,0 @@
-# defined function to show charge mouse and keyboard info
-#
-function charge --description 'show charge mouse and keyboard info'
-    echo Charge Mouse: (upower -i (upower -e | grep mouse) | grep percentage | awk '{ print $2 }')
-    echo Charge Keyboard: (upower -i (upower -e | grep keyboard) | grep percentage | awk '{ print $2 }')
-end
-
