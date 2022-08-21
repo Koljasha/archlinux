fdaf267cc95927442201a49835fe4fb665523590
Koljasha <koljasha@mail.ru>
2022-08-21 20:43:36 +0300

    samba-server.fish status

--- config/fish/functions/samba-server.fish
+++ config/fish/functions/samba-server.fish
@@ -1,10 +1,14 @@
 # defined smb.service start|stop
 #
 function samba-server --description 'smb.service start|stop'
-        if test -z (pgrep -x smbd)
-            sudo systemctl start smb.service
+        if test \( (count $argv) -eq 1 \) -a \( "$argv[1]" = 'status' \)
+            systemctl status smb.service
         else
-            sudo systemctl stop smb.service
+            if test -z (pgrep -x smbd)
+                sudo systemctl start smb.service
+            else
+                sudo systemctl stop smb.service
+            end
         end
 end
 
