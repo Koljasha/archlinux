34f8eca6cd071e661e154596fcfab5642e7e08b1
Koljasha <koljasha@mail.ru>
2022-09-03 15:23:02 +0300

    rm zenity

--- config/openbox/menu_exit_openbox.sh
+++ config/openbox/menu_exit_openbox.sh
@@ -6,9 +6,9 @@ if (( $# != 1 )); then
 fi
 
 if [[ $1 == 'reboot' ]]; then
-    zenity --question --no-wrap --title "Reboot PC" --text "Do you want to REBOOT your PC?" && systemctl -i reboot
+    systemctl -i reboot
 elif [[ $1 == 'poweroff' ]]; then
-    zenity --question --no-wrap --title "Power Off PC" --text "Do you want to POWER OFF your PC?" && systemctl -i poweroff
+    systemctl -i poweroff
 else
     echo "./menu_exit_openbox.sh reboot  or  ./menu_exit_openbox.sh poweroff  !!!"
 fi
