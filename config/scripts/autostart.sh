abc1ca42487fe76ed1915baab132f1f7dac21901
Koljasha <koljasha@mail.ru>
2022-09-05 16:41:29 +0300

    scripts add autostart.sh

---
+++ config/scripts/autostart.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+#
+# change config values for brightness, eth, wifi
+#
+
+# polybar - change display output
+disp=`xrandr | grep "connected primary" | cut -d" " -f1`
+sed -i "s/--output.*--brightness/--output $disp --brightness/" ~/.config/scripts/brightness.sh
+
+# ethernet interface
+ethernet=`nmcli device | grep 'ethernet' | awk '{ print $1 }'`
+line=`grep -n 'module/eth' ~/.config/polybar/i3.conf | cut -d':' -f1`
+line=$((line+2))
+sed -i -e "${line}s/interface = .*/interface = ${ethernet}/" ~/.config/polybar/i3.conf
+
+# wifi interface
+wifi=`nmcli device | grep 'wifi' | awk '{ print $1 }'`
+line=`grep -n 'module/wlan' ~/.config/polybar/i3.conf | cut -d':' -f1`
+line=$((line+2))
+sed -i -e "${line}s/interface = .*/interface = ${wifi}/" ~/.config/polybar/i3.conf
+
+# ethernet interface
+ethernet=`nmcli device | grep 'ethernet' | awk '{ print $1 }'`
+line=`grep -n 'module/eth' ~/.config/polybar/openbox.conf | cut -d':' -f1`
+line=$((line+2))
+sed -i -e "${line}s/interface = .*/interface = ${ethernet}/" ~/.config/polybar/openbox.conf
+
+# wifi interface
+wifi=`nmcli device | grep 'wifi' | awk '{ print $1 }'`
+line=`grep -n 'module/wlan' ~/.config/polybar/openbox.conf | cut -d':' -f1`
+line=$((line+2))
+sed -i -e "${line}s/interface = .*/interface = ${wifi}/" ~/.config/polybar/openbox.conf
+
