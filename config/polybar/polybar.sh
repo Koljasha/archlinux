2196e6638a01a3785ce5476285fbb423b8b191a5
Koljasha <koljasha@mail.ru>
2022-09-05 16:42:50 +0300

    polybar scripts

---
+++ config/polybar/polybar.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+wm=$1
+
+if [[ $wm != "openbox" ]] && [[ $wm != "i3" ]]; then
+    echo "No config name"
+    exit 1
+fi
+
+# Terminate already running bar instances
+killall -q polybar
+
+# change config values for brightness, eth, wifi
+~/.config/scripts/autostart.sh
+
+# Launch bars
+echo "---" | tee -a /tmp/polybar.log
+polybar -c ~/.config/polybar/$wm.conf polybar >>/tmp/polybar.log 2>&1 &
+
+echo "Bars launched..."
+
