f72e5fcbbf24643768b53863478da9b036140576
Koljasha <koljasha@mail.ru>
2022-10-25 13:55:18 +0300

    qtile mouse button4 (wheel up) to window.bring_to_front

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -188,7 +188,7 @@ keys = [
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([mod], "Button4", lazy.window.bring_to_front())
 ]
 
 
