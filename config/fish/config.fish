2c9133b0a9d4d23b189e9937368516cdfe9c9449
Koljasha <koljasha@mail.ru>
2022-12-17 22:53:50 +0300

    bashrc, fish.config : -v for files command

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,7 +8,10 @@ set fish_greeting
 prompt_command
 
 alias ll="ls -lah"
-alias grep='grep --color=auto'
+alias grep="grep --color=auto"
+alias cp="cp -v"
+alias mv="mv -v"
+alias rm="rm -v"
 
 # alias wg-up="sudo wg-quick up wg0"
 # alias wg-down="sudo wg-quick down wg0"
