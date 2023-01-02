3012f4560fac1d019ffa19ff503b7a1bf5d4c824
Koljasha <koljasha@mail.ru>
2023-01-02 18:40:36 +0300

    fish rg, fd commands

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,6 +13,9 @@ alias cp="cp -v"
 alias mv="mv -v"
 alias rm="rm -v"
 
+alias fd="fd --hidden --no-ignore"
+alias rg="rg --hidden --no-ignore --ignore-case"
+
 # alias wg-up="sudo wg-quick up wg0"
 # alias wg-down="sudo wg-quick down wg0"
 # alias wg-status="sudo wg"
