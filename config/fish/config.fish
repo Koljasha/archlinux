ccf2c05ae277ead7bb051942cbe901c3bdbe53bb
Koljasha <koljasha@mail.ru>
2023-01-03 00:54:42 +0300

    fish add openvpn-up|down|status

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,5 +18,16 @@ alias rg="rg --hidden --no-ignore --ignore-case"
 
 # alias wg-up="sudo wg-quick up wg0"
 # alias wg-down="sudo wg-quick down wg0"
-# alias wg-status="sudo wg"
+# function wg-status --description 'show WireGuard status'
+    # if test -z (sudo wg | sed -n 1p)
+        # echo 'No WireGuard Connection'
+    # else
+        # echo -e 'WireGuard Connect\n'
+        # sudo wg
+    # end
+# end
+
+# alias openvpn-up="nmcli connection up OpenVpn"
+# alias openvpn-down="nmcli connection down OpenVpn"
+# alias openvpn-status="nmcli connection show"
 
