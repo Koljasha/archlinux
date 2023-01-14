0e2d722f7870fbd12444a5d2ee2e6f25a6e23a0b
Koljasha <koljasha@mail.ru>
2023-01-14 21:42:57 +0300

    scripts: change volume.sh

--- config/scripts/volume.sh
+++ config/scripts/volume.sh
@@ -6,30 +6,30 @@
 
 ###
 # round Volume for past in rofi -select
+#
+# substring - ${string:position:length}
+# length - ${#string}
 ###
 volume=`pactl list sinks | grep "Volume: front-" | cut -d'/' -f2 | cut -d'%' -f1 | sed 's/^[[:space:]]*//' \
         | awk '{ if ($1 < 10) print "10"; else if ($1 > 100) print "100"; else print $1 }'`
 
-checker=`echo $volume | awk '{ print $1/10 }'| grep -o "\."`
-
-if [[ $checker == '.' ]]; then
-    integer=`echo $volume | awk '{ print $1/10 }'| cut -d'.' -f1`
-    fractional=`echo $volume | awk '{ print $1/10 }'| cut -d'.' -f2`
-    case $fractional in
-        1|2)
-            volume="${integer}0"
-            ;;
-        3|4|5|6|7)
-            volume="${integer}5"
-            ;;
-        8|9)
-            integer=$((integer+1))
-            volume="${integer}0"
-            ;;
-        *)
-            ;;
-    esac
-fi
+last=${volume: -1:1}
+prelast=${volume: -2:1}
+other=${volume:0:${#volume}-2}
+case $last in
+    1|2)
+        volume="${other}${prelast}0"
+        ;;
+    3|4|5|6|7)
+        volume="${other}${prelast}5"
+        ;;
+    8|9)
+        prelast=$((prelast+1))
+        volume="${other}${prelast}0"
+        ;;
+    *)
+        ;;
+esac
 ###
 
 declare -a options=(
