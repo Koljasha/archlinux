d95761298b0e9645ec400bf5477f38611abbde63
Koljasha <koljasha@mail.ru>
2023-04-14 11:04:46 +0300

    xbindkeys comment autostart

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -42,6 +42,9 @@ fi
 # Change values for brightness, eth, wifi
 ~/.config/scripts/default_values.sh &
 
+# Additionally bindings
+# xbindkeys &
+
 # Set mouse params | show devices: `xinput list`
 # ~/.config/scripts/mouse.sh set "Logitech MX Ergo" &
 # ~/.config/scripts/mouse.sh set "BT4.0+2.4G Mouse" -0.90 &
--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -36,6 +36,9 @@ fi
 # Change values for brightness, eth, wifi
 ~/.config/scripts/default_values.sh &
 
+# Additionally bindings
+# xbindkeys &
+
 # Set mouse params | show devices: `xinput list`
 # ~/.config/scripts/mouse.sh set "Logitech MX Ergo" &
 # ~/.config/scripts/mouse.sh set "BT4.0+2.4G Mouse" -0.90 &
