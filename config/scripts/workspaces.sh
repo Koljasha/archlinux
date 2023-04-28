24c0176c72434c0909d856457e97e9658404b756
Koljasha <koljasha@mail.ru>
2023-04-28 21:36:22 +0300

    qtile: workspaces script

---
+++ config/scripts/workspaces.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#
+# change Workspaces | move to Workspaces
+#
+
+# current Workspaces
+if [[ -n $(pgrep -f '^/usr/bin/python /usr/bin/qtile') ]]; then
+    workspaces=`qtile cmd-obj -o group -f info | grep 'label' | cut -d: -f2 | cut -d\' -f2`
+else
+    exit 2
+fi
+
+declare -a options=(
+"1\0icon\x1fvideo-display"
+"2\0icon\x1fvideo-display"
+"3\0icon\x1fvideo-display"
+"4\0icon\x1fvideo-display"
+"5\0icon\x1fvideo-display"
+"6\0icon\x1fvideo-display"
+"7\0icon\x1fvideo-display"
+"8\0icon\x1fvideo-display"
+"9\0icon\x1fvideo-display"
+"10\0icon\x1fvideo-display"
+)
+
+# change Workspaces
+if (( $# == 1 )) && [[ $1 == 'change' ]]; then
+    workspaces=`printf '%b\n' "${options[@]}" \
+                | rofi -dmenu -l 3 -select $workspaces -p Workspaces 2>/dev/null`
+    xdotool key --clearmodifiers super+$workspaces
+# move to Workspaces
+elif (( $# == 1 )) && [[ $1 == 'move' ]]; then
+    workspaces=`printf '%b\n' "${options[@]}" \
+                | rofi -dmenu -l 3 -select $workspaces -p "Move to Workspaces" 2>/dev/null`
+    xdotool key --clearmodifiers super+shift+$workspaces
+else
+    echo 'Error: Invalid arguments [ change | move ]'
+    exit 2
+fi
+
