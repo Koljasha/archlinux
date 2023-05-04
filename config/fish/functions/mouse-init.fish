0632e875662a972b0002ed0da2435fe16b504ebb
Koljasha <koljasha@mail.ru>
2023-05-04 13:18:06 +0300

    fish: samba-server

--- config/fish/functions/mouse-init.fish
+++
@@ -1,8 +0,0 @@
-# mouse init
-#
-function mouse-init --description 'init mouse params'
-    # Set mouse params | show devices: `xinput list`
-    ~/.config/scripts/mouse.sh set "Logitech MX Ergo"
-    ~/.config/scripts/mouse.sh set "BT4.0+2.4G Mouse" -0.90
-end
-
--- config/fish/functions/samba-server.fish
+++ config/fish/functions/samba-server.fish
@@ -1,16 +1,14 @@
 # defined smb.service start|stop|status
 #
 function samba-server --description 'smb.service start|stop|status'
-        if test (count $argv) -eq 0
-            if test -z (pgrep -x smbd)
+        if test \( (count $argv) -eq 1 \) -a \( "$argv[1]" = 'start' \)
                 sudo systemctl start smb.service
-            else
+        else if test \( (count $argv) -eq 1 \) -a \( "$argv[1]" = 'stop' \)
                 sudo systemctl stop smb.service
-            end
         else if test \( (count $argv) -eq 1 \) -a \( "$argv[1]" = 'status' \)
             systemctl status smb.service
         else
-            echo "Use `samba-server` for start|stop; `samba-server status` for status "
+            echo "Use: samba-server start|stop|status"
         end
 end
 
