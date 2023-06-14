2686d560d3d3522dc52fc170531576d233db9b92
Koljasha <koljasha@mail.ru>
2023-06-14 18:27:34 +0300

    Orbit Fusion settings

--- config/scripts/xbindkeys.sh
+++ config/scripts/xbindkeys.sh
@@ -5,12 +5,14 @@
 #
 
 deft_pro=`xinput list | grep 'pointer' | grep -i 'Deft Pro'`
-huge=`xinput list | grep 'pointer' | grep -i 'Huge'`
+orbit_fusion=`xinput list | grep 'pointer' | grep -i 'Orbit Fusion'`
 
 killall xbindkeys 2>/dev/null
 
 if [[ $deft_pro != '' ]]; then
     xbindkeys -f ~/.config/xbindkeys/xbindkeysrc_deft_pro
+elif [[ $orbit_fusion != '' ]]; then
+    xbindkeys -f ~/.config/xbindkeys/xbindkeysrc_orbit_fusion
 else
     xbindkeys -f ~/.config/xbindkeys/xbindkeysrc
 fi
