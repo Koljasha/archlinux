ae705083f85a01728e67f7e75187959b15c3ac0c
Koljasha <koljasha@mail.ru>
2023-06-14 16:30:19 +0300

    fish: pass-import|export fix for work with links

--- config/fish/functions/pass-export.fish
+++ config/fish/functions/pass-export.fish
@@ -2,9 +2,11 @@
 #
 function pass-export --description 'export pass passwords'
     if test -d ~/.password-store
-        tar czvf pass.tar.gz --directory=$(xdg-user-dir) .password-store/
+        mkdir passwords
+        cp -r ~/.password-store/. passwords/
+        tar czvf pass.tar.gz passwords/
         gpg -r $(whoami) -e pass.tar.gz
-        rm -v pass.tar.gz
+        rm -rf pass.tar.gz passwords/
         echo "Export file has been created"
     else
         echo "No pass store"
--- config/fish/functions/pass-import.fish
+++ config/fish/functions/pass-import.fish
@@ -4,10 +4,11 @@ function pass-import --description 'import pass passwords'
 	if test -f pass.tar.gz.gpg
 		gpg --decrypt pass.tar.gz.gpg > pass.tar.gz
 		if test $status -eq 0
-			tar xzvf pass.tar.gz
-			rm -rfv ~/.password-store/
-			mv -v .password-store/ ~/
-			rm -rfv pass.tar.*
+			tar xvf pass.tar.gz
+			rm -rf ~/.password-store
+			mkdir ~/.password-store
+			cp -r passwords/. ~/.password-store/
+			rm -rf pass.tar.* passwords/
 			echo "Passwords imported"
 		else
 			echo "Invalid decryption"
