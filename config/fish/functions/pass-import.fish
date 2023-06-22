d6cbbeee11b0320fc50117fc5c43003f4179ed0a
Koljasha <koljasha@mail.ru>
2023-06-22 21:54:37 +0300

    pass-import bug fix: work with ln -s folder

--- config/fish/functions/pass-import.fish
+++ config/fish/functions/pass-import.fish
@@ -5,8 +5,12 @@ function pass-import --description 'import pass passwords'
 		gpg --decrypt pass.tar.gz.gpg > pass.tar.gz
 		if test $status -eq 0
 			tar xvf pass.tar.gz
-			rm -rf ~/.password-store
-			mkdir ~/.password-store
+			if test -d ~/.password-store
+				rm -rf ~/.password-store/*
+				rm -rf ~/.password-store/.*
+			else
+				mkdir ~/.password-store
+			end
 			cp -r passwords/. ~/.password-store/
 			rm -rf pass.tar.* passwords/
 			echo "Passwords imported"
