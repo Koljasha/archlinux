2e41a998587bfa3692428e6d298001cb9fa1b94c
Koljasha <koljasha@mail.ru>
2024-08-02 16:51:16 +0300

    venv: upgrade pip

--- config/fish/functions/venv.fish
+++ config/fish/functions/venv.fish
@@ -5,6 +5,7 @@ function venv --description 'create/activate python virtual environment'
         source .venv/bin/activate.fish
     else
         python -m venv .venv; source .venv/bin/activate.fish
+        pip install --upgrade pip
     end
 end
 
